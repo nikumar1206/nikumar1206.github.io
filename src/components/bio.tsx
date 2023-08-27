37291efea9703d4cb30d6cdc87c163a924494bc1
Nikhil <nikumar1206@gmail.com>
2023-08-27 14:08:52 -0400

    removed bio: on splash page

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -3,17 +3,14 @@ import NeuButton from "./button";
 const Bio = () => {
 	return (
 		<section className="mx-auto mt-[3rem] flex flex-col gap-y-4">
-			<div>
-				<h2 className="headers">Bio:</h2>
-				<span className="">
-					I&apos;m a software engineer at the Machine Learning Center of
-					Excellence (MLCoE) at
-					<span className="text-[var(--blue)] inline"> JPMorgan Chase</span>.
-					Passionate about cloud computing, machine learning, and building
-					performant backends. In my free time, I enjoy playing tennis,
-					weightlifting, and spending time with my family.
-				</span>
-			</div>
+			<span className="">
+				I&apos;m a software engineer at the Machine Learning Center of
+				Excellence (MLCoE) at
+				<span className="text-[var(--blue)] inline"> JPMorgan Chase</span>.
+				Passionate about cloud computing, machine learning, and building
+				performant backends. In my free time, I enjoy playing tennis,
+				weightlifting, and spending time with my family.
+			</span>
 			<div className="flex justify-end gap-x-3 w-[90%]">
 				<a
 					href="https://drive.google.com/file/d/1Dcz1nOLtwLO9bXxG7dD64-GCE5xsaA07/view?usp=sharing"
