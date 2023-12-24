20e16ad9607ca444ccc2ca5eb5e7779fef68fa24
Nikhil <nikumar1206@gmail.com>
2023-12-23 22:59:46 -0500

    trying new chunking strategy and font downloaded

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,22 @@ import react from "@vitejs/plugin-react-swc";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
+import webfontDownload from "vite-plugin-webfont-dl";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		webfontDownload([
+			"https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800&display=swap",
+		]),
+	],
 	server: {
 		host: true,
 	},
+
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
@@ -20,5 +26,18 @@ export default defineConfig({
 	build: {
 		outDir: "build",
 		emptyOutDir: true,
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					if (id.includes("react")) {
+						return "react";
+					} else if (id.includes("node_modules")) {
+						return "vendor";
+					} else {
+						return "index";
+					}
+				},
+			},
+		},
 	},
 });
