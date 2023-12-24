73c2ea026962c95b99c27b2df8ee9136da8712fa
Nikhil <nikumar1206@gmail.com>
2023-12-23 23:08:47 -0500

    removing unecessary deps, better splitting, no more downloading font

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import react from "@vitejs/plugin-react-swc";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-import webfontDownload from "vite-plugin-webfont-dl";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		webfontDownload([
-			"https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800&display=swap",
-		]),
-	],
+	plugins: [react()],
 	server: {
 		host: true,
 	},
@@ -31,8 +25,10 @@ export default defineConfig({
 				manualChunks: (id) => {
 					if (id.includes("react")) {
 						return "react";
-					} else if (id.includes("node_modules")) {
-						return "vendor";
+					} else if (id.includes("node_modules") && id.length % 2 === 0) {
+						return "vendor-1";
+					} else if (id.includes("node_modules") && id.length % 2 !== 0) {
+						return "vendor-2";
 					} else {
 						return "index";
 					}
