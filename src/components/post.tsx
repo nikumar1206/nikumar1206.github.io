ce956d13aad42ed92338a0a6971b998258c0abdd
Nikhil <nikumar1206@gmail.com>
2023-12-25 17:03:55 -0500

    small css changes

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -5,125 +5,125 @@ import { PostData } from "../shared/posts";
 import { CodeBlock } from "./codeBlock";
 
 interface PostParams {
-	readonly id?: string;
-	post: PostData | null;
+  readonly id?: string;
+  post: PostData | null;
 }
 
 const Post = ({ post }: PostParams) => {
-	const { theme } = useTheme();
+  const { theme } = useTheme();
 
-	const isDark = theme === "dark";
-	const [markdownContent, setMarkdownContent] = useState<string>(
-		post?.body ?? ""
-	);
+  const isDark = theme === "dark";
+  const [markdownContent, setMarkdownContent] = useState<string>(
+    post?.body ?? "",
+  );
 
-	useEffect(() => {
-		const fetchMarkdownFile = (id: number) => {
-			let markdownContent = "";
-			console.log("fetching markdown file");
-			fetch(`/post${id}.md`)
-				.then(async (res) => {
-					markdownContent = await res.text();
-					setMarkdownContent(markdownContent);
-				})
-				.catch((err) => {
-					console.log(err);
-					markdownContent = "An error occurred while fetching the post.";
-				});
+  useEffect(() => {
+    const fetchMarkdownFile = (id: number) => {
+      let markdownContent = "";
+      console.log("fetching markdown file");
+      fetch(`/post${id}.md`)
+        .then(async (res) => {
+          markdownContent = await res.text();
+          setMarkdownContent(markdownContent);
+        })
+        .catch((err) => {
+          console.log(err);
+          markdownContent = "An error occurred while fetching the post.";
+        });
 
-			return markdownContent;
-		};
-		if (post && !post.body) {
-			const markdownContent = fetchMarkdownFile(post.id);
-			setMarkdownContent(markdownContent);
-			console.log(markdownContent);
-		}
-	}, [post]);
+      return markdownContent;
+    };
+    if (post && !post.body) {
+      const markdownContent = fetchMarkdownFile(post.id);
+      setMarkdownContent(markdownContent);
+      console.log(markdownContent);
+    }
+  }, [post]);
 
-	if (!post) {
-		return (
-			<div className="max-w-[64rem] mx-auto p-6 mt-10 font-posts">
-				<h1 className="text-4xl font-bold">404</h1>
-				<p className="text-xl mt-4">Post not found.</p>
-			</div>
-		);
-	}
+  if (!post) {
+    return (
+      <div className="max-w-[64rem] mx-auto p-6 mt-10 font-posts">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p className="text-xl mt-4">Post not found.</p>
+      </div>
+    );
+  }
 
-	return (
-		<div
-			className={`max-w-[64rem] mx-auto p-6 ${
-				isDark ? "" : ""
-			} mt-10 font-posts`}
-		>
-			<span className="float-right">{post.date}</span>
-			<ReactMarkdown
-				className="max-w-none prose-pre:p-0 prose-h2:mt-[1em] prose-h1:text-5xl prose prose-fuschia dark:prose-invert"
-				components={{
-					img({ ...props }) {
-						return (
-							<div className="flex mx-auto flex-col">
-								<img
-									{...props}
-									className="m-0 mx-auto w-[45%] h-[25%] rounded-md"
-								/>
-								<span className="text-center">
-									{/* eslint-disable-next-line */}
-									{props.alt!}
-								</span>
-							</div>
-						);
-					},
-					a({ ...props }) {
-						return (
-							<span className="inline-flex">
-								<a
-									{...props}
-									className=" hover:underline transition-all duration-200"
-									target="_blank"
-									rel="noopener noreferrer"
-								/>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="12"
-									height="12"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									className="align-middle ml-[.15rem]"
-								>
-									<path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
-									<path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
-								</svg>
-							</span>
-						);
-					},
-					code({ inline, className, children, ...props }) {
-						const match = /language-(\w+)/.exec(className ?? "");
-						return !inline && match ? (
-							<CodeBlock
-								language={match[1] ? match[1] : ""}
-								value={String(children).replace(/\n$/, "")}
-								{...props}
-							/>
-						) : (
-							<code
-								{...props}
-								style={{ color: isDark ? "#ff63c3" : "#e60073" }}
-								className={className}
-							>
-								{children}
-							</code>
-						);
-					},
-				}}
-			>
-				{markdownContent}
-			</ReactMarkdown>
-		</div>
-	);
+  return (
+    <div
+      className={`max-w-[64rem] mx-auto p-6 ${
+        isDark ? "" : ""
+      } mt-10 font-posts`}
+    >
+      <span className="float-right">{post.date}</span>
+      <ReactMarkdown
+        className="max-w-none prose-pre:p-0 prose-h2:mt-[1em] prose-h1:text-5xl prose prose-fuschia dark:prose-invert"
+        components={{
+          img({ ...props }) {
+            return (
+              <div className="flex mx-auto flex-col">
+                <img
+                  {...props}
+                  className="m-0 mx-auto w-[45%] h-[25%] rounded-md"
+                />
+                <span className="text-center">
+                  {/* eslint-disable-next-line */}
+                  {props.alt!}
+                </span>
+              </div>
+            );
+          },
+          a({ ...props }) {
+            return (
+              <span className="inline-flex">
+                <a
+                  {...props}
+                  className=" hover:underline transition-all duration-200"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="align-middle ml-[.15rem]"
+                >
+                  <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                  <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+                </svg>
+              </span>
+            );
+          },
+          code({ inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className ?? "");
+            return !inline && match ? (
+              <CodeBlock
+                language={match[1] ? match[1] : ""}
+                value={String(children).replace(/\n$/, "")}
+                {...props}
+              />
+            ) : (
+              <code
+                {...props}
+                style={{ color: isDark ? "#ff63c3" : "#e60073" }}
+                className={className}
+              >
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {markdownContent}
+      </ReactMarkdown>
+    </div>
+  );
 };
 
 export default Post;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,109 +1,88 @@
 /** @type {import('tailwindcss').Config} */
 import plugin from "tailwindcss/plugin";
-const Myclass = plugin(function ({ addUtilities }) {
-	addUtilities({
-		".my-rotate-y-180": {
-			transform: "rotateY(180deg)",
-		},
-		".preserve-3d": {
-			transformStyle: "preserve-3d",
-		},
-		".perspective": {
-			perspective: "1000px",
-		},
-		".backface-hidden": {
-			backfaceVisibility: "hidden",
-		},
-	});
-});
-
 export default {
-	darkMode: ["class"],
-	content: [
-		"./index.html",
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
-	],
-	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
-		extend: {
-			colors: {
-				border: "hsl(var(--border))",
-				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				primary: {
-					DEFAULT: "var(--dteal)",
-					foreground: "hsl(var(--primary-foreground))",
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
-				},
-				muted: {
-					DEFAULT: "hsl(178, 40%, 67%)",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))",
-				},
-				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
-				},
-			},
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)",
-			},
-			keyframes: {
-				"accordion-down": {
-					from: { height: 0 },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: 0 },
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-			},
-			typography: {
-				DEFAULT: {
-					css: {
-						maxWidth: "100ch", // add required value here
-					},
-				},
-			},
-			fontFamily: {
-				posts: ["IBM Plex Sans", "sans-serif"],
-			},
-		},
-	},
-	plugins: [
-		require("tailwindcss-animate"),
-		require("@tailwindcss/typography"),
-		Myclass,
-	],
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "var(--dteal)",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(178, 40%, 67%)",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: "100ch", // add required value here
+          },
+        },
+      },
+      fontFamily: {
+        posts: ["IBM Plex Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
